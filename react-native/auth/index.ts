defd6acf7b3649d34d9e56a730bbc4b86bb27046
Viktor Pishuk <59969484+vpishuk@users.noreply.github.com>
2025-04-01 23:12:52 -0400

    feat(auth): adds useSignInAnonymously hook (#75)

--- react-native/auth/index.ts
+++ react-native/auth/index.ts
@@ -11,3 +11,4 @@ export * from "./useIdToken";
 export * from "./useReauthenticateWitCredentialMutation";
 export * from "./useReauthenticateWitRedirectMutation";
 export * from "./useAuthStateReady";
+export * from "./useSignInAnonymouslyMutation";
