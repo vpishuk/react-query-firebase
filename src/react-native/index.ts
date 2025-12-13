5ba1b2c466cc082d41e6c18d4efec36559133910
Victor Pishuk <victor.pishuk@gmail.com>
2025-12-12 19:46:14 -0500

    fixes package.json configuration to properly export public interfaces

--- src/react-native/index.ts
+++ src/react-native/index.ts
@@ -1,1 +1,5 @@
 export * from "./context/index.js";
+export * from "./analytics/index.js";
+export * from "./auth/index.js";
+export * from "./firestore/index.js";
+export * from "./remoteConfig/index.js";
